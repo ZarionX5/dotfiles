75cf6134d2abf9f73db8601006a88256e46398f0
ZarionX5 <81365126+ZarionX5@users.noreply.github.com>
2026-06-09 20:21:27 +0300

    add docker mirrors

--- system/services/docker.nix
+++ system/services/docker.nix
@@ -1,3 +1,6 @@
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    extraOptions = "--registry-mirror=https://dockerhub.timeweb.cloud --registry-mirror=https://huecker.io --registry-mirror=https://mirror.gcr.io --registry-mirror=https://c.163.com --registry-mirror=https://registry.docker-cn.com --registry-mirror=https://daocloud.io";
+  };
 }
